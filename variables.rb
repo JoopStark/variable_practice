1911716b352afc9b7537ccd2676ff06138d39013
Joop Stark <stark.joop@gmail.com>
2023-05-12 14:13:22 -0600

    Add strings

---
+++ variables.rb
@@ -0,0 +1,5 @@
+first = "Who woods these are I think I know"
+second = "His house is in the village though"
+third = "He will not see me stopping her here"
+forth = "To watch his woods fill up with snow"
+
