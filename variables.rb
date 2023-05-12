442d58e26418aaf91ce399456111caeef3a106ec
Joop Stark <stark.joop@gmail.com>
2023-05-12 14:16:44 -0600

    Add integers

--- variables.rb
+++ variables.rb
@@ -3,3 +3,8 @@ second = "His house is in the village though"
 third = "He will not see me stopping her here"
 forth = "To watch his woods fill up with snow"
 
+num = 12
+neg_num = -12
+my_age = 33
+height_inches = 70
+
